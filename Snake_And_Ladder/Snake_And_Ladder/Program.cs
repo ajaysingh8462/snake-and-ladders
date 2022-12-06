270f2f13336e8a6d083d2f21a0331334fc747833
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-06 10:15:12 +0530

    [ajay]- player position at 0

---
+++ Snake_And_Ladder/Snake_And_Ladder/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Snake_And_Ladder;
+using System;
+
+namespace SnakeLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********Snake and Ladder game*********");
+            SnakeLadder game = new SnakeLadder();
+            game.Board();
+        }
+    }
+}
---
+++ Snake_And_Ladder/Snake_And_Ladder/SnakeLadder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    internal class SnakeLadder
+    {
+        public void Start()
+        {
+            //implementing single player start at 0.
+            //variables
+            int position = 0;
+            int playerOne;
+            //initialize player
+            playerOne = position;
+            Console.WriteLine($"player One position is {playerOne}");
+        }
+
+        public void Board()
+        {
+            //at the start
+            Start();
+        }
+    }
+}
